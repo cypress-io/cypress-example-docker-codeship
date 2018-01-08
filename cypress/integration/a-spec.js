61a5cdbb5e55cc116ba09df5ae9e3a98f53d23d5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-08 15:59:51 -0500

    add cy.exec

--- cypress/integration/a-spec.js
+++ cypress/integration/a-spec.js
@@ -1,12 +1,16 @@
 describe('a-spec', () => {
   describe('page', () => {
     beforeEach(() => {
-      debugger
       cy.visit(Cypress.env('HOST') || 'index.html')
     })
 
     it('has h2', () => {
       cy.contains('h2', 'test')
+    })
+
+    it('echoes', () => {
+      cy.exec('echo Jane Lane')
+        .its('stdout').should('contain', 'Jane Lane')
     })
   })
 })
